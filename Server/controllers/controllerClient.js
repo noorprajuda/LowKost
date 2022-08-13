1e6c37a34fe5bc51623fa48ec4137e240ebcbd4a
rivaldiheriyan27 <60634705+rivaldiheriyan27@users.noreply.github.com>
2022-08-13 16:06:01 +0700

    feat: data client (must be check)

--- Server/controllers/controllerClient.js
+++ Server/controllers/controllerClient.js
@@ -10,6 +10,7 @@ const {
   Images,
   MyBooking,
   Rules,
+  Facilities,
   Users,
 } = require("../models");
 const {
@@ -18,6 +19,7 @@ const {
   hashPassword,
   compareHash,
 } = require("../helpers/helpers");
+const { MIDTRANS_SERVER_KEY } = process.env;
 
 class ControllerClient {
   static async registerClient(req, res, next) {
@@ -64,11 +66,7 @@ class ControllerClient {
         },
       });
 
-      let facilitiy = await BoardingHouseFacilities.findAll({
-        include: [{ models: Facilities, attributes: ["name"] }, { kos }],
-      });
-
-      console.log(kos);
+      res.status(200).json(kos);
     } catch (err) {
       console.log(err);
       next(err);
@@ -77,7 +75,160 @@ class ControllerClient {
 
   static async bourdingHousesId(req, res, next) {
     try {
-    } catch (err) {}
+      const { id } = req.params;
+
+      const idBourdingHousesId = await BoardingHouses.findByPk(id, {
+        include: [
+          { model: Categories, attributes: ["name"] },
+          { model: City, attributes: ["name"] },
+          {
+            model: Users,
+            attributes: {
+              exclude: ["createdAt", "updatedAt", "password"],
+            },
+          },
+          { model: Images, attributes: ["imgUrl"] },
+          {
+            model: BoardingHouseFacilities,
+            include: [{ model: Facilities, attributes: ["name"] }],
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+          },
+          {
+            model: BoardingHouseRules,
+            include: [{ model: Rules, attributes: ["name"] }],
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+          },
+        ],
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      });
+      // const facility = await BoardingHouseFacilities.findAll({
+      //   where: { BoardingHouseId: id },
+      //   include: { model: Facilities, attributes: ["name"] },
+      //   attributes: {
+      //     exclude: ["createdAt", "updatedAt"],
+      //   },
+      // });
+
+      // const rull = await BoardingHouseRules.findAll({
+      //   where: { BoardingHouseId: id },
+      //   include: { model: Rules, attributes: ["name"] },
+      //   attributes: {
+      //     exclude: ["createdAt", "updatedAt"],
+      //   },
+      // });
+
+      res.status(200).json(idBourdingHousesId);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async myBookmark(req, res, next) {
+    try {
+      const { id } = req.params;
+      const myBookmark = await Bookmarks.findAll({
+        where: {
+          UserId: id,
+        },
+        include: {
+          model: BoardingHouses,
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          },
+        },
+      });
+      res.status(200).json(myBookmark);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async createBookmark(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { id: UserId } = req.user;
+
+      const bookmarkFind = await Bookmarks.findByPk(id);
+
+      if (!bookmarkFind) {
+        throw { name: "NotFound" };
+      }
+
+      let input = {
+        UserId: UserId,
+        BoardingHouseId: id,
+      };
+
+      const makeBookmark = await Bookmarks.create(input);
+      res.status(201).json({ message: "Succesfully add bookmark" });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async deleteBookmark(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      const findBookmark = await Bookmarks.findByPk(id);
+
+      if (!findBookmark) {
+        throw { name: "NotFound" };
+      }
+
+      await Bookmarks.destroy({
+        where: {
+          id,
+        },
+      });
+
+      res.status(200).json({
+        message: "Bookmark succesfully delete",
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async payment(req, res, next) {
+    try {
+      const { price } = req.body;
+
+      let snap = new midtransClient.Snap({
+        // Set to true if you want Production Environment (accept real transaction).
+        isProduction: false,
+        serverKey: process.env.MIDTRANS_SERVER_KEY,
+      });
+
+      let parameter = {
+        transaction_details: {
+          order_id: Math.floor(Math.random() * 10000000000000),
+          gross_amount: price,
+        },
+        credit_card: {
+          secure: true,
+        },
+        customer_details: {
+          email: req.findUser.email,
+        },
+      };
+
+      const transaction = await snap.createTransaction(parameter);
+      let transactionToken = transaction.token;
+
+      res.status(201).json({
+        statusCode: 201,
+        token: transactionToken,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
