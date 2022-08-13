b5e04d342dab639a4719fe297a2e37e7e0e36738
noruhm <henryhrty@gmail.com>
2022-08-13 16:36:37 +0700

    test google api

--- Server/controllers/OwnerController.js
+++ Server/controllers/OwnerController.js
@@ -8,7 +8,13 @@ const {
   City,
   Rules,
   BoardingHouseRules,
+  sequelize,
+  Sequelize,
 } = require("../models");
+const googleMapsClient = require("@google/maps").createClient({
+  key: process.env.GOOGLE_MAPS_API,
+  Promise: Promise,
+});
 
 module.exports = class OwnerController {
   static async registerHandler(req, res, next) {
@@ -19,7 +25,7 @@ module.exports = class OwnerController {
         fullName,
         email,
         password,
-        phoneNumber,
+        phoneNumber: `0${phoneNumber}`,
         address,
         role,
       });
@@ -35,6 +41,7 @@ module.exports = class OwnerController {
     try {
       const boardinghouses = await BoardingHouses.findAll({
         include: [{ model: Categories }, { model: City }],
+        where: { UserId: req.user.id },
       });
       res.status(200).json(boardinghouses);
     } catch (err) {
@@ -45,21 +52,86 @@ module.exports = class OwnerController {
   static async getBoardingHousesById(req, res, next) {
     try {
       const boardinghouses = await BoardingHouses.findByPk(req.params.id, {
-        include: [{ model: Categories }, { model: City }],
+        include: [
+          { model: Categories, attributes: ["name"] },
+          { model: City, attributes: ["name"] },
+          { model: Images, attributes: ["imgUrl"] },
+          {
+            model: BoardingHouseFacilities,
+            include: [{ model: Facilities, attributes: ["name"] }],
+          },
+          {
+            model: BoardingHouseRules,
+            include: [{ model: Rules, attributes: ["name"] }],
+          },
+          { model: Users, attributes: ["fullName", "email"] },
+        ],
       });
-      const images = await Images.findAll({
-        where: { BoardingHouseId: boardinghouses.id },
-      });
-      const facilites = await BoardingHouseFacilities.findAll({
-        where: { BoardingHouseId: boardinghouses.id },
-        include: [{ model: Facilities }],
-      });
-      const rules = await BoardingHouseRules.findAll({
-        where: { BoardingHouseId: boardinghouses.id },
-        include: [{ model: Rules }],
-      });
-      res.status(200).json({ boardinghouses, rules, images, facilites });
+      res.status(200).json(boardinghouses);
     } catch (err) {
+      next(err);
+    }
+  }
+
+  static async addBoardingHouse(req, res, next) {
+    const t = await sequelize.transaction();
+    try {
+      const {
+        name,
+        price,
+        CategoryId,
+        CityId,
+        totalRoom,
+        description,
+        mainImg,
+        address,
+        StackRules,
+        StackImages,
+        StackFacilities,
+      } = req.body;
+      let latlong = "";
+      const response = await googleMapsClient
+        .geocode({
+          address,
+        })
+        .asPromise();
+      let jsn = response.json.results;
+      for (let i = 0; i < jsn.length; i++) {
+        let res = jsn[i];
+        latlong += res.geometry.location.lat + " " + res.geometry.location.lng;
+      }
+
+      const newBoardingHouse = await BoardingHouses.create(
+        {
+          name,
+          price,
+          CategoryId,
+          CityId,
+          totalRoom,
+          description,
+          mainImg,
+          address,
+          UserId: req.user.id,
+          location: Sequelize.fn("ST_GeomFromText", `POINT(${latlong})`),
+        },
+        { transaction: t }
+      );
+      const rules = StackRules.map((rule) => {
+        return { BoardingHouseId: newBoardingHouse.id, RuleId: rule.id };
+      });
+      await BoardingHouseRules.bulkCreate(rules, { transaction: t });
+      const images = await StackImages.map((img) => {
+        return { imgUrl: img.imgUrl, BoardingHouseId: newBoardingHouse.id };
+      });
+      await Images.bulkCreate(images, { transaction: t });
+      const facilities = StackFacilities.map((el) => {
+        return { FaciltyId: el.id, BoardingHouseId: newBoardingHouse.id };
+      });
+      await BoardingHouseFacilities.bulkCreate(facilities, { transaction: t });
+      await t.commit();
+      res.status(200).json({ message: "Successfull add new Kos" });
+    } catch (err) {
+      await t.rollback();
       next(err);
     }
   }
