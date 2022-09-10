e41abbbd7d0d6895ed84c2cb546dce588f90aa25
noorprajuda <marsetio.noor@gmail.com>
2022-09-10 07:52:47 +0700

    feat: Deploy

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,7 @@ const errorHandler = require("./middlewares/errorHandlers");
 const path = require("path");
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
